a4600fe88882b8fb1da2d594372cb1cfa2ea00fd
aaaAa <158430839+Atuatu-Hhakumai@users.noreply.github.com>
2024-05-06 16:55:38 +0900

    Update

--- js/loading.js
+++ js/loading.js
@@ -1,16 +1,16 @@
 const loading = document.querySelector(".loading");
 const loadingInner = document.querySelector(".loading-content");
 
+// 二回目以降の訪問の場合、ローディングを少し短くする
 function isFirstTimeVisit() {
   var isFirstTime = sessionStorage.getItem("isFirstTimeVisit");
   if (!isFirstTime) {
       sessionStorage.setItem("isFirstTimeVisit", "true");
       return true;
-  } else {
-      return false;
-  }
+  } else{ return false; }
 }
 
+// 二回目以降の訪問の場合、ローディングを少し短くする
 if (isFirstTimeVisit()) {
   const loadingTl = gsap.timeline();
   loadingTl.to(loading, {
@@ -22,7 +22,6 @@ if (isFirstTimeVisit()) {
       duration: 1.2,
       autoAlpha: 0
   });
-  
   const counter = document.getElementById('counter');
   gsap.to(counter, {
     delay: 0.7,
@@ -45,7 +44,6 @@ if (isFirstTimeVisit()) {
       duration: 1,
       autoAlpha: 0
   });
-  
   const counter = document.getElementById('counter');
   gsap.to(counter, {
     duration: 1.3,
@@ -56,4 +54,4 @@ if (isFirstTimeVisit()) {
       counter.innerHTML = Math.round(this.targets()[0].innerHTML);
     }
   });
-}
+};
